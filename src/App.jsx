3306ae01b87245393ede0661e80a11f37dfad5b0
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-05-21 14:02:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <p className="text-3xl">Text</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
